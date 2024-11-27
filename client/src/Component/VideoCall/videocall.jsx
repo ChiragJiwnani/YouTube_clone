61fb440edc637bcafab0f00a75087cd02be9d8d1
Chirag Jiwnani <98516754+ChiragJiwnani@users.noreply.github.com>
2024-11-27 17:21:41 +0530

    Update videocall.jsx vercel

--- client/src/Component/VideoCall/videocall.jsx
+++ client/src/Component/VideoCall/videocall.jsx
@@ -37,7 +37,8 @@ const VideoCall = () => {
     });
 
   useEffect(() => {
-    socket.current = io.connect("https://youtube-clone-3ge8.onrender.com/");
+    socket.current = io.connect("https://youtubeclone-server.vercel.app/");
+    // socket.current = io.connect("https://youtube-clone-3ge8.onrender.com/");
     // socket.current = io.connect("http://localhost:5000/");
     console.log("Use Effect running");
 
