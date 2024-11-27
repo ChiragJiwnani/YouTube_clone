2a0843f529c2b36442924a60d0ea9c41f50fda51
Chirag Jiwnani <98516754+ChiragJiwnani@users.noreply.github.com>
2024-11-27 17:19:08 +0530

    Update chat.jsx vercel

--- client/src/Component/Chat/chat.jsx
+++ client/src/Component/Chat/chat.jsx
@@ -6,7 +6,7 @@ import io from "socket.io-client";
 import CryptoJS from "crypto-js";
 import "./chat.css";
 
-const socket = io("https://youtube-clone-3ge8.onrender.com");
+const socket = io("https://youtubeclone-server.vercel.app");
 
 const Chat = ({ roomId, userId, userName }) => {
   const dispatch = useDispatch();
